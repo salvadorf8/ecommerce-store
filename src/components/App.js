9e894b3436e7f5f2a932cc8915599582f48beaae
salvadorf8 <aztecsalvador@gmail.com>
2020-06-30 23:23:42 -0500

    First Commit w/initial hardcoded HomePage & SCSS style

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react';
+
+import HomePage from './HomePage.component';
+
+function App() {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
+}
+
+export default App;
