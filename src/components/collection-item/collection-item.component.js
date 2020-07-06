d7681c131d2ab43c7cbf9a52c1c7d5e965e6b658
salvadorf8 <aztecsalvador@gmail.com>
2020-07-06 14:59:29 -0500

    styled item components to have button over image - added opacity - added hover color changes

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -2,6 +2,8 @@ import './collection-item.styles.scss';
 
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 const CollectionItem = ({ name, price, imageUrl }) => (
     <div className='collection-item'>
         <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,6 +11,7 @@ const CollectionItem = ({ name, price, imageUrl }) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton inverted>Add to cart</CustomButton>
     </div>
 );
 
