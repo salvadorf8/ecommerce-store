0738de6279ed5597db61ddc3acf501d6c01c9607
salvadorf8 <aztecsalvador@gmail.com>
2020-07-08 19:58:51 -0500

    code implementing redux-persist library to store state in windows local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,26 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+// below is importing the type of storage we need, below is the actual localStorage object from the window object
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+// below is the persist config - only thing we want to persist is the cart - user is handled by google auth
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
