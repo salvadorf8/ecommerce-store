2b7db7de16fcc92607e103db6e8f334741e9fe09
salvadorf8 <aztecsalvador@gmail.com>
2020-07-08 20:01:15 -0500

    refactored code to open all related category items instead of just 5 items of each - note: shop.selectors.js is sample of using ID_MAP

--- src/pages/homepage/home-page.component.js
+++ src/pages/homepage/home-page.component.js
@@ -1,12 +1,12 @@
 import './home-page.styles.scss';
 import React from 'react';
 
-import DirectoryMenu from '../../components/directory-menu/directory.component';
+import Directory from '../../components/directory-menu/directory.component';
 
 const HomePage = () => {
     return (
         <div className='homepage'>
-            <DirectoryMenu />
+            <Directory />
         </div>
     );
 };
