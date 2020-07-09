2c47d25c6dfdd44a1e435b6836dd11fc26d77288
salvadorf8 <aztecsalvador@gmail.com>
2020-07-09 11:59:42 -0500

    added new selector to return an Array (using Object.keys) from the converted Object so that the app will work

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,9 +1,13 @@
 import { createSelector } from 'reselect';
 
-// map so when id are numbers but the apps values are string - this is called data normalization
+// map so when id are numbers but the apps values are string - this is called data normalization - converting array to Objects (Hash Table data structure)
+// it is better for searching for items than Array.  this is a common computing optimization when talking about data structures.
 
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
+
+// since Array was converted to Object, coded this because Object.keys will return an array of objects so app will continue to work
+export const selectCollectionsForPreview = createSelector([selectCollections], (collections) => Object.keys(collections).map((key) => collections[key]));
 
 export const selectCollection = (collectionUrlParam) => createSelector([selectCollections], (collections) => collections[collectionUrlParam]);
